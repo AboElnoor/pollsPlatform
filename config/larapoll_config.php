084fd47b40075219b15f9ad940493f28dbef1ece
Mahmoud AboElnour <mahmoud.ezzat@arabyads.com>
2019-05-19 15:44:32 +0200

    change show poll view

--- config/larapoll_config.php
+++ config/larapoll_config.php
@@ -4,7 +4,7 @@ return [
     'admin_guard' => env('LARAPOLL_ADMIN_AUTH_GUARD', 'web'),
     'pagination' => env('LARAPOLL_PAGINATION', 15),
     'prefix' => env('LARAPOLL_PREFIX', 'admin_polls'),
-    'single_poll' => '',
+    'single_poll' => 'poll',
     'results' => '',
     'radio' => 'polls.radio',
     'checkbox' => 'polls.checkbox',
